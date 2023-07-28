7a08db8de5886e8b578fe300e9be0e46a04ac7a6
eniodev <enioblvck@g.mail.com>
2023-07-28 02:45:43 -0300

    started responsivity and animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ec-planos';
+
+  ngOnInit() {
+    AOS.init();
+  }
+
+  isMenuVisible: boolean = false;
+
+  toggleMenu() {
+    this.isMenuVisible = !this.isMenuVisible;
+  }
+
+  @ViewChild('menu', { static: true }) menu!: ElementRef;
+
+  closeMenu() {
+    const menuToggle = document.getElementById('menu-toggle') as HTMLInputElement;
+    menuToggle.checked = false;
+    /*
+    const menu = this.menu.nativeElement;
+    menu.classList.remove('menu-visible');*/
+    this.isMenuVisible = false;
+
+  }
 }
